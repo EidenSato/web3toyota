2e5abe75d32a2ed433367c7298a6d344f680e141
EidenSato <125642739+EidenSato@users.noreply.github.com>
2023-03-15 15:13:50 +0900

    Add files via upload

---
+++ client/abi.js
@@ -0,0 +1,150 @@
+var abi = {
+  hasoto: [
+    {
+      "anonymous": false,
+      "inputs": [
+        {
+          "indexed": false,
+          "internalType": "uint256",
+          "name": "_points",
+          "type": "uint256"
+        }
+      ],
+      "name": "NewLetter",
+      "type": "event"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "",
+          "type": "address"
+        }
+      ],
+      "name": "lastTime",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "name": "letters",
+      "outputs": [
+        {
+          "internalType": "address",
+          "name": "from",
+          "type": "address"
+        },
+        {
+          "internalType": "address",
+          "name": "to",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "points",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "time",
+          "type": "uint256"
+        },
+        {
+          "internalType": "string",
+          "name": "text",
+          "type": "string"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_to",
+          "type": "address"
+        },
+        {
+          "internalType": "string",
+          "name": "_text",
+          "type": "string"
+        }
+      ],
+      "name": "postLetter",
+      "outputs": [],
+      "stateMutability": "nonpayable",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_adr",
+          "type": "address"
+        }
+      ],
+      "name": "timePoints",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [],
+      "name": "totalLetters",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    },
+    {
+      "inputs": [
+        {
+          "internalType": "address",
+          "name": "_adr",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "startNumber",
+          "type": "uint256"
+        }
+      ],
+      "name": "totalPoints",
+      "outputs": [
+        {
+          "internalType": "uint256",
+          "name": "",
+          "type": "uint256"
+        }
+      ],
+      "stateMutability": "view",
+      "type": "function"
+    }
+  ]
+}
+
---
+++ client/address.js
@@ -0,0 +1,42 @@
+//Shibuya
+var addresslist= [
+    {
+        "name": "佐藤　総一",
+        "address": "0x0a4aB49Cb3a71263BB3881799f50c32Af0Dba651"
+    },
+    {
+        "name": "大山　茂",
+        "address": "0xFCDB19aaebc25c8a608eE848dcD9546bb77C390c"
+    },
+    {
+        "name": "三上　さやか",
+        "address": "0x74D21Bea65d6F3bd770845038663Eb04c803B12f"
+    },
+    {
+        "name": "尾崎　常道",
+        "address": "0xEd551eE941781CEDED449EBdB26F8d47B54149f8"
+    },
+    {
+        "name": "濱田　淳",
+        "address": "0x18769354a755Bc3AEaA18E324d669b86DDfb585C"
+    },
+    {
+        "name": "内山　貴司",
+        "address": "0x6303F4c12d8d78222135E6212BF005C47c81F205"
+    }
+]
+//Ganache
+var addresslist_ganache = [
+    {
+        "name": "佐藤　総一",
+        "address": "0xA4A529b5257A9eA942e87b9Fc9169254905AAb96"
+    },
+    {
+        "name": "鈴木　太郎",
+        "address": "0xd091703b6493ae60F7912DbB50d216F140Ae9a2a"
+    },
+    {
+        "name": "山田　花子",
+        "address": "0x9b0C697f28fdb2d83AdAaFe86A5098880e9B52f8"
+    }
+]
